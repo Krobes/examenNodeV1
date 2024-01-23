f07c680e861920cf9702a793494958155066da26
rbonilla <rafa_lara@hotmail.es>
2024-01-23 12:28:33 +0100

    Inicialización del código del examen de Node en Express
    Versión 1 operativa

---
+++ helpers/multiplicar.js
@@ -0,0 +1,11 @@
+function multiplicar(base){
+    let tabla = [];
+    for(let i = 1; i<=10; i++){
+        tabla.push(i * base);
+    }
+    return tabla;
+}
+
+module.exports = {
+    multiplicar
+}
